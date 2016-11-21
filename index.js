73a7367a52dab4e9285f295b60598adb1755b4f3
steffanboodhoo <steffan_boodhoo@hotmail.com>
2016-11-21 15:07:49 -0400

    basic angular2

---
+++ index.js
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import {BrowserModule } from 'Aangular/platform-browser';
+
+@NgModule{
+	imports: [ BrowserModule ]
+}
+
+export class AppModule {}
+
+// console.log('hello')
